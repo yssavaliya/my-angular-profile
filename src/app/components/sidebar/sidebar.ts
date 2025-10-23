014db4385f58f1a36d4403c140e0d8a1837ca2d1
yssavaliya <87963173+yssavaliya@users.noreply.github.com>
2025-10-23 10:56:44 +0530

    Revert "feat: add dynamic section component with routing and navigation data"

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -11,18 +11,11 @@ import { LucideAngularModule, Sun, Moon } from 'lucide-angular';
 export class Sidebar implements OnInit {
   @Input() data: any;
   @Input() toggleTheme!: () => void;
-  @Input() isDarkMode!: boolean;
+
+  @Input() isDarkMode!: boolean; // <- now receives from parent
 
   SunIcon = Sun;
   MoonIcon = Moon;
 
   ngOnInit() { }
-
-  // Compute initials from name (first letters of first two words)
-  get initials(): string {
-    if (!this.data?.name) return 'U';
-    const words = this.data.name.split(' ');
-    if (words.length === 1) return words[0].charAt(0).toUpperCase();
-    return (words[0].charAt(0) + words[1].charAt(0)).toUpperCase();
-  }
 }
